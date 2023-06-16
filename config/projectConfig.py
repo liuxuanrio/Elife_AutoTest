0b3441658c79cef98bf7ac8ed6f6e3aea45b71f1
liuliuxuan <1072460053@qq.com>
2023-06-16 14:23:07 +0800

    test19

--- config/projectConfig.py
+++ config/projectConfig.py
@@ -8,7 +8,7 @@ class ProjectConfig:
         :return:
         """
         config = {
-            "driver_app": {"caseName": "driver_app_ui_testcase.xlsx", "runType": "driver_app_Ride_More1.mqt",
+            "driver_app": {"caseName": "driver_app_ui_testcase.xlsx", "runType": "driver_app_employee_emailLogin.mqt",
                            "testData": "/data/test_case_file/driver_app/", "testCase": "/test/case/driver_app_case/"}
         }
         return config
