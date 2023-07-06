1dbd8162abbd89499ab175dcede39976ef9dca5e
liuliuxuan <1072460053@qq.com>
2023-07-06 11:30:56 +0800

    test37

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class Test_merchants_go:
 
         # 保存用例中的变量信息
         with allure.step("variable"):
-            allure.attach(msg[2], 'Json数据', allure.attachment_type.JSON)
+            allure.attach(msg[2].json(), 'Json数据', allure.attachment_type.JSON)
 
         with allure.step("截图"):
             # 获取当前用例生成的截图
